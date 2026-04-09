56df4e3461093550706c7ea7ad873417e46ae7d8
Chidinma Nwaeze <chidinmabnwaeze@gmail.com>
2026-04-09 17:24:50 +0100

    logo

--- app/(employer)/requests/page.tsx
+++ app/(employer)/requests/page.tsx
@@ -50,6 +50,11 @@ import { useEffect, useState } from "react";
 
 export default function RequestsPage() {
   const [requests, setRequests] = useState([]);
+
+  const [activeTab, setActiveTab]= useState<"all"| "closed" | "drafts">('all')
+
+  const all = activeTab === "all";
+
   useEffect(() => {
     const fetchRequests = async () => {
       try {
@@ -88,7 +93,7 @@ export default function RequestsPage() {
       {/* tabs */}
       <section className="mt-6 border-b border-gray-200">
         <div>
-          <button className="px-4 py-2 border-b-2 border-[#FF0046] text-[#FF0046] font-medium">
+          <button className="px-4 py-2 border-b-2 border-[#FF0046] text-[#FF0046] font-medium" onClick={()=> setActiveTab(all)}>
             Active Requests
           </button>
           <button className="px-4 py-2 border-b-2 border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300">
